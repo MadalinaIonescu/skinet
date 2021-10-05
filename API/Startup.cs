98e22124c088297d188a464c585036b22015bbed
ionescu <madalina.ionescu209@gmail.com>
2021-10-05 16:23:44 +0300

    End Of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -17,12 +18,16 @@ namespace API
             _config = configuration;
         }
 
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        //do the dependency injections -- on the build is called, the kestrel is doing this
+        //in this method is not important the order of these commands
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //lifetime of an http request, whn it creates a controller it creates this repository,
+            //when the controller is destroyed, it destroys this too
+            services.AddScoped<IProductRepository, ProductRepository>(); 
             services.AddControllers();
+            //we did that when setting up the connection string
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("Default")));
                 
@@ -33,6 +38,8 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //configure the middleware pipeline -- on the run , the kestrel is doing this
+        //in this method is important the order of these commands
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
